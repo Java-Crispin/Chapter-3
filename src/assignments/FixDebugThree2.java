c688d0eaa37ccd6ae61877176540f0ab4cc43a88
Aidan Crispin <acrispin5466@gmial.com>
2019-09-13 10:15:44 -0500

    Classwork for colege

--- src/assignments/FixDebugThree2.java
+++ src/assignments/FixDebugThree2.java
@@ -6,16 +6,15 @@ public class FixDebugThree2
     {
         int a = 2, b = 5, c = 10;
         add(b, c);
-        add(b, c);
         subtract(a,b);
     }
-    public static void add(int a, double  b)
+    public static void add(int b, int c)
     {
-        System.out.println("The sum of " + a +
-                " and " + b + " is " + (a + b));
+        System.out.println("The sum of " + c +
+                " and " + b + " is " + (b + c));
     }
     public static void subtract(int a, int b){
         System.out.println("The difference between " +
-                a + " and " + b + " is " +  (a - b));
+                a + " and " + b + " is " +  (b - a));
     }
 }
